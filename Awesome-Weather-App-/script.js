178168ed4ad4b9ffecabb67c09f95bffe475d55e
ChristopherGatt <110685887+ChristopherGatt@users.noreply.github.com>
2022-09-22 23:17:28 +1000

    Add files via upload

--- Awesome-Weather-App-/script.js
+++ Awesome-Weather-App-/script.js
@@ -1,5 +1,13 @@
 const apiKey = "0720d4712f70a776f7e730c2791ecd1c";
 const city = "sydney";
+const city1 = "brisbane";
+const city2 = "melbourne";
+const city3 = "hobart";
+const city4 = "darwin";
+const city5 = "perth";
+const city6 = "canberra";
+const city7 = "adelaide";
+
 const url1 =
   "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 //"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
