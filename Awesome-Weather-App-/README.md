b76b8cd5d3dff9db0f98e8d93fed6eab5b756c4c
ChristopherGatt <110685887+ChristopherGatt@users.noreply.github.com>
2022-10-14 12:30:26 +1100

    Add files via upload

--- Awesome-Weather-App-/README.md
+++ Awesome-Weather-App-/README.md
@@ -1,1 +1,34 @@
 # Awesome-Weather-App-
+
+User Story
+
+AS A traveler
+
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+Acceptance Criteria
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+1. First I add basic html elements and create div's, id's, classes and add links
+
+2. layout and stlye css sheet
+
+3. Add API's from openweather.net to js file
+
+4. create functions for calling the API's and its data
+
+5. Add this function as a click event to both the searchbutton and side buttons
+
+6. The data will appear in the corresponding location
+
+7.
--- Awesome-Weather-App-/script.js
+++ Awesome-Weather-App-/script.js
@@ -1,4 +1,41 @@
+var searchEngine = document.getElementById("searchBtn");
+
+var searchInput = document.getElementById("searchh");
+
+var searchBtns = document.getElementById("cities.string.value");
+
 const apiKey = "0720d4712f70a776f7e730c2791ecd1c";
+
+function fiveDay(lat, lon) {
+  var fiveForecast =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    apiKey;
+  fetch(fiveForecast)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
+  //document.getElementById("temp1").textContent = data.list[1].main.temp;
+}
+
+/*array (cityo) = [
+  sydney,
+  brisbane,
+  melbourne,
+  hobart,
+  darwin,
+  perth,
+  canberra,
+  adelaide,
+];*/
+
 const city = "sydney";
 const city1 = "brisbane";
 const city2 = "melbourne";
@@ -8,26 +45,47 @@ const city5 = "perth";
 const city6 = "canberra";
 const city7 = "adelaide";
 
-const url1 =
+/*const url1 =
   "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-//"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+//"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";*/
+function getWeather() {
+  const url3 =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchInput.value +
+    "&appid=" +
+    apiKey +
+    "&units=metric";
 
-const url2 =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&appid=" +
-  apiKey;
+  fetch(url3)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.coord.lat);
+      console.log(data.coord.lon);
 
-//const url3 = "https://pokeapi.co/api/v2/pokemon/ditto";
+      document.getElementById("currentTemp").textContent = data.main.temp;
+      document.getElementById("cityName").textContent =
+        data.name + "(" + data.timezone + ")";
+      document.getElementById("currentHumidity").textContent =
+        data.main.humidity;
+      document.getElementById("currentWind").textContent = data.wind.speed;
 
-fetch(url2)
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
+      fiveDay(data.coord.lat, data.coord.lon);
+    });
+}
 
-    document.getElementById("currentTemp").textContent = data.main.temp;
-    document.getElementById("cityName").textContent = data.name;
-  });
+function searchCity() {
+  getWeather();
+}
+
+searchEngine.addEventListener("click", searchCity);
+
+/*const url2 =
+  "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";*/
+
+function sideBtns() {}
+
+searchBtns.addEventListener("click", searchCity);
